24c9587e9fc93b1c90d399bb3bf635607e4e2ead
maikealcantara <maikerdralcantara@gmail.com>
2025-10-22 19:22:35 -0300

    feat(model): add Client entity and unit tests for getters/setters

---
+++ src/main/java/com/mqped/fims/model/Client.java
@@ -0,0 +1,88 @@
+package com.mqped.fims.model;
+
+import java.time.LocalDateTime;
+
+public class Client {
+    private String id;
+    private String name;
+    private String cpf;
+    private LocalDateTime birthDate;
+    private String motherName;
+    private String cnpj;
+    private String genre;
+    private LocalDateTime createdAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", genre='" + genre +
+                '}';
+    }
+}
