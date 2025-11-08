9f60824a1f8f0c55586b86ba4ae1e8b038760ba1
maikealcantara <maikerdralcantara@gmail.com>
2025-11-08 17:00:35 -0300

    feat(exceptions): Add new exceptions

---
+++ src/main/java/com/mqped/fims/exceptions/TokenExpiredException.java
@@ -0,0 +1,11 @@
+package com.mqped.fims.exceptions;
+
+public class TokenExpiredException extends RuntimeException {
+    public TokenExpiredException(String message) {
+        super(message);
+    }
+
+    public TokenExpiredException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
