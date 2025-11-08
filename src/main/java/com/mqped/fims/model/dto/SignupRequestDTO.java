c63db2152854dd6611117b1221992cb7eb0a6494
maikealcantara <maikerdralcantara@gmail.com>
2025-11-08 16:03:01 -0300

    feat(dto): Add request and response DTOs

---
+++ src/main/java/com/mqped/fims/model/dto/SignupRequestDTO.java
@@ -0,0 +1,65 @@
+package com.mqped.fims.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+public class SignupRequestDTO {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100)
+    private String password;
+
+    private Set<String> roles;
+
+    public SignupRequestDTO() {
+    }
+
+    public SignupRequestDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
