b76208fe3599b68f9d717f234ba41ee58691e1e6
maikealcantara <maikerdralcantara@gmail.com>
2025-11-08 23:19:01 -0300

    update(config): Set modules to not be available during tests

--- src/main/java/com/mqped/fims/config/UserInitializer.java
+++ src/main/java/com/mqped/fims/config/UserInitializer.java
@@ -11,12 +11,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Set;
 
 @Configuration
+@Profile("!test")
 public class UserInitializer {
 
     private static final Logger userInitializerLogger = LoggerFactory.getLogger(UserInitializer.class);
